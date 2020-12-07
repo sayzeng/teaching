4b19af167c2377c32408dacbe5112d23014f5bdd
Sarah Zeng <69217632+sayzeng@users.noreply.github.com>
2020-12-07 09:21:15 -0800

    Update seatingchart0.py

--- seatingchart0.py
+++ seatingchart0.py
@@ -6,31 +6,33 @@ Created on Mon Apr 30 20:37:55 2018
 
 INPUT
 Sheet 1: 3 columns where 
-column 1 is the alpha/letter component of the seat number
-column 2 is the numbering start for the row
-column 3 is the numbering end for the row
+    column 1 is the alpha/letter component of the seat number
+    column 2 is the numbering start for the row
+    column 3 is the numbering end for the row
 Sheet 2: 1 column of left-handed seat numbers (alphanumeric)
 """
 
 import pandas as pd
 
-room="PETER108"
+room = "PETER108"
 
-filename=room+'.xlsx'
-df=pd.read_excel(filename, header=None, sheet_name=0)
-leftlist=pd.read_excel(filename, header=None, sheet_name=1)
-leftlist=leftlist[0].tolist()
+# Input room seating details
+filename = room + '.xlsx'
+df = pd.read_excel(filename, header=None, sheet_name=0)
+leftlist = pd.read_excel(filename, header=None, sheet_name=1)
 
-#Make list of seats from chart
-tags=[]
-for i in range(0,len(df)):
-    seats=list(range(df.iloc[i,1],df.iloc[i,2]+1))
-    prefix=[df.iloc[i,0]]*len(seats)
-    tags=tags+[m+str(n) for m,n in zip(prefix,seats)]
+# Get seats from chart (list to Series)
+seats = []
+for i in range(len(df)):
+    prefix, rowstart, rowend = df.iloc[i, 0:3]
+    seats += (prefix + str(n) for n in range(rowstart, rowend + 1))
+seats = pd.Series(seats)
 
-#Export for random assignment
-s=pd.Series(tags)
-lefty=s.isin(leftlist)
-dfe=pd.concat([s,lefty], axis=1)
-dfe.columns = ['Seat','Lefty']
-dfe.to_excel(room+'_tags.xlsx', index=False)
+# Export for random assignment
+lefty = seats.isin(leftlist)
+
+dfe = pd.concat([seats, lefty], axis=1)
+dfe.columns = ['Seat', 'Lefty']
+dfe.to_excel(room + '_tags.xlsx', index=False)
+
+print('Code complete.')
